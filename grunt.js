4c5860eaa80f990ebb100761984e6e08e836dbf9
azeem <kr00r4n@gmail.com>
2013-01-06 00:37:12 +0530

    Added handlebars

--- grunt.js
+++ grunt.js
@@ -3,7 +3,15 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({    
     coffeelint: {
-      app: ['src/js/*.coffee']
+      app: {
+        files: ['src/js/*.coffee'],
+        options: {
+          indentation: {
+            value: 4,
+            level: 'warn'
+          }
+        }
+      }
     },
 
     coffee: {
@@ -25,19 +33,33 @@ module.exports = function(grunt) {
       }
     },
 
+    handlebars: {
+        compile: {
+            options: {
+                namespace: 'JST',
+                wrapped: true,
+                processName: function(filename) {
+                    var splits = filename.split('/');
+                    splits = splits[splits.length-1].split('.');
+                    var name = splits[0];
+                    return name;
+                }
+            },
+            files: {
+                'build/js/templates.js': ['src/templates/*.hbs']
+            }
+        }
+    },
+
     copy: {
       target: {
         files: {
           'build/': 'src/*.html',
+          'build/js/lib/': "src/js/lib/*.js",
           'build/css/blueprint/': 'blueprint-css/blueprint_build/*'
         }
       }
     },
-
-    // clean: {
-    //   build: ['build'],
-    //   options: {}
-    // }
 
   });
 
@@ -46,7 +68,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-coffeelint');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
-//  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-handlebars');
 
   // Default task.
   grunt.registerTask('default', 'coffeelint coffee less copy');
